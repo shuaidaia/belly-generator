d2f3bfbdeaecbe618477fea4f71d0db79b58125f
shuaidaia <2976834933@qq.com>
2024-02-13 21:53:40 +0800

    完成项目主页开发 commit 4

--- antd-generator/src/pages/Index/index.tsx
+++ antd-generator/src/pages/Index/index.tsx
@@ -1,164 +1,183 @@
-import { PageContainer } from '@ant-design/pro-components';
-import { useModel } from '@umijs/max';
-import { Card, theme } from 'antd';
-import React from 'react';
+import { listGeneratorVoByPageUsingPost } from '@/services/backend/generatorController';
+import { UserOutlined } from '@ant-design/icons';
+import { PageContainer, ProFormSelect, ProFormText, QueryFilter } from '@ant-design/pro-components';
+import { Avatar, Card, Flex, Image, Input, List, message, Tabs, Tag, Typography } from 'antd';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
 
 /**
- * 每个单独的卡片，为了复用样式抽成了组件
- * @param param0
- * @returns
+ * 默认分页参数
  */
-const InfoCard: React.FC<{
-  title: string;
-  index: number;
-  desc: string;
-  href: string;
-}> = ({ title, href, index, desc }) => {
-  const { useToken } = theme;
-
-  const { token } = useToken();
-
-  return (
-    <div
-      style={{
-        backgroundColor: token.colorBgContainer,
-        boxShadow: token.boxShadow,
-        borderRadius: '8px',
-        fontSize: '14px',
-        color: token.colorTextSecondary,
-        lineHeight: '22px',
-        padding: '16px 19px',
-        minWidth: '220px',
-        flex: 1,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          gap: '4px',
-          alignItems: 'center',
-        }}
-      >
-        <div
-          style={{
-            width: 48,
-            height: 48,
-            lineHeight: '22px',
-            backgroundSize: '100%',
-            textAlign: 'center',
-            padding: '8px 16px 16px 12px',
-            color: '#FFF',
-            fontWeight: 'bold',
-            backgroundImage:
-              "url('https://gw.alipayobjects.com/zos/bmw-prod/daaf8d50-8e6d-4251-905d-676a24ddfa12.svg')",
-          }}
-        >
-          {index}
-        </div>
-        <div
-          style={{
-            fontSize: '16px',
-            color: token.colorText,
-            paddingBottom: 8,
-          }}
-        >
-          {title}
-        </div>
-      </div>
-      <div
-        style={{
-          fontSize: '14px',
-          color: token.colorTextSecondary,
-          textAlign: 'justify',
-          lineHeight: '22px',
-          marginBottom: 8,
-        }}
-      >
-        {desc}
-      </div>
-      <a href={href} target="_blank" rel="noreferrer">
-        了解更多 {'>'}
-      </a>
-    </div>
-  );
+const DEFAULT_PAGE_PARAMS: PageRequest = {
+  current: 1,
+  pageSize: 4,
+  sortField: 'createTime',
+  sortOrder: 'descend',
 };
 
+/**
+ * 主页
+ * @constructor
+ */
 const Index: React.FC = () => {
-  const { token } = theme.useToken();
-  const { initialState } = useModel('@@initialState');
+  const [loading, setLoading] = useState<boolean>(true);
+  const [dataList, setDataList] = useState<API.GeneratorVO[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  // 搜索条件
+  const [searchParams, setSearchParams] = useState<API.GeneratorQueryRequest>({
+    ...DEFAULT_PAGE_PARAMS,
+  });
+
+  /**
+   * 搜索
+   */
+  const doSearch = async () => {
+    setLoading(true);
+    try {
+      const res = await listGeneratorVoByPageUsingPost(searchParams);
+      setDataList(res.data?.records ?? []);
+      setTotal(Number(res.data?.total) ?? 0);
+    } catch (error: any) {
+      message.error('获取数据失败，' + error.message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    doSearch();
+  }, [searchParams]);
+
+  /**
+   * 标签列表
+   * @param tags
+   */
+  const tagListView = (tags?: string[]) => {
+    if (!tags) {
+      return <></>;
+    }
+
+    return (
+      <div style={{ marginBottom: 8 }}>
+        {tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </div>
+    );
+  };
 
   return (
-    <PageContainer>
-      <Card
-        style={{
-          borderRadius: 8,
-        }}
-        bodyStyle={{
-          backgroundImage:
-          // @ts-ignore
-            initialState?.settings?.navTheme === 'realDark'
-              ? 'background-image: linear-gradient(75deg, #1A1B1F 0%, #191C1F 100%)'
-              : 'background-image: linear-gradient(75deg, #FBFDFF 0%, #F5F7FF 100%)',
+    <PageContainer title={<></>}>
+      <Flex justify="center">
+        <Input.Search
+          style={{
+            width: '40vw',
+            minWidth: 320,
+          }}
+          placeholder="搜索代码生成器"
+          allowClear
+          enterButton="搜索"
+          size="large"
+          onChange={(e) => {
+            searchParams.searchText = e.target.value;
+          }}
+          onSearch={(value: string) => {
+            setSearchParams({
+              ...searchParams,
+              ...DEFAULT_PAGE_PARAMS,
+              searchText: value,
+            });
+          }}
+        />
+      </Flex>
+      <div style={{ marginBottom: 16 }} />
+
+      <Tabs
+        size="large"
+        defaultActiveKey="newest"
+        items={[
+          {
+            key: 'newest',
+            label: '最新',
+          },
+          {
+            key: 'recommend',
+            label: '推荐',
+          },
+        ]}
+        onChange={() => {}}
+      />
+
+      <QueryFilter
+        span={12}
+        labelWidth="auto"
+        labelAlign="left"
+        defaultCollapsed={false}
+        style={{ padding: '16px 0' }}
+        onFinish={async (values: API.GeneratorQueryRequest) => {
+          setSearchParams({
+            ...DEFAULT_PAGE_PARAMS,
+            // @ts-ignore
+            ...values,
+            searchText: searchParams.searchText,
+          });
         }}
       >
-        <div
-          style={{
-            backgroundPosition: '100% -30%',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '274px auto',
-            backgroundImage:
-              "url('https://gw.alipayobjects.com/mdn/rms_a9745b/afts/img/A*BuFmQqsB2iAAAAAAAAAAAAAAARQnAQ')",
-          }}
-        >
-          <div
-            style={{
-              fontSize: '20px',
-              color: token.colorTextHeading,
-            }}
-          >
-            欢迎使用 Ant Design Pro
-          </div>
-          <p
-            style={{
-              fontSize: '14px',
-              color: token.colorTextSecondary,
-              lineHeight: '22px',
-              marginTop: 16,
-              marginBottom: 32,
-              width: '65%',
-            }}
-          >
-            Ant Design Pro 是一个整合了 umi，Ant Design 和 ProComponents
-            的脚手架方案。致力于在设计规范和基础组件的基础上，继续向上构建，提炼出典型模板/业务组件/配套设计资源，进一步提升企业级中后台产品设计研发过程中的『用户』和『设计者』的体验。
-          </p>
-          <div
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              gap: 16,
-            }}
-          >
-            <InfoCard
-              index={1}
-              href="https://umijs.org/docs/introduce/introduce"
-              title="了解 umi"
-              desc="umi 是一个可扩展的企业级前端应用框架,umi 以路由为基础的，同时支持配置式路由和约定式路由，保证路由的功能完备，并以此进行功能扩展。"
-            />
-            <InfoCard
-              index={2}
-              title="了解 ant design"
-              href="https://ant.design"
-              desc="antd 是基于 Ant Design 设计体系的 React UI 组件库，主要用于研发企业级中后台产品。"
-            />
-            <InfoCard
-              index={3}
-              title="了解 Pro Components"
-              href="https://procomponents.ant.design"
-              desc="ProComponents 是一个基于 Ant Design 做了更高抽象的模板组件，以 一个组件就是一个页面为开发理念，为中后台开发带来更好的体验。"
-            />
-          </div>
-        </div>
-      </Card>
+        <ProFormSelect label="标签" name="tags" mode="tags" />
+        <ProFormText label="名称" name="name" />
+        <ProFormText label="描述" name="description" />
+      </QueryFilter>
+
+      <div style={{ marginBottom: 24 }} />
+
+      <List<API.GeneratorVO>
+        rowKey="id"
+        loading={loading}
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 2,
+          md: 3,
+          lg: 3,
+          xl: 4,
+          xxl: 4,
+        }}
+        dataSource={dataList}
+        pagination={{
+          current: searchParams.current,
+          pageSize: searchParams.pageSize,
+          total,
+          onChange(current: number, pageSize: number) {
+            setSearchParams({
+              ...searchParams,
+              current,
+              pageSize,
+            });
+          },
+        }}
+        renderItem={(data) => (
+          <List.Item>
+            <Card hoverable cover={<Image alt={data.name} src={data.picture} />}>
+              <Card.Meta
+                title={<a>{data.name}</a>}
+                description={
+                  <Typography.Paragraph ellipsis={{ rows: 2 }} style={{ height: 44 }}>
+                    {data.description}
+                  </Typography.Paragraph>
+                }
+              />
+              {tagListView(data.tags)}
+              <Flex justify="space-between" align="center">
+                <Typography.Text type="secondary" style={{ fontSize: 12 }}>
+                  {moment(data.createTime).fromNow()}
+                </Typography.Text>
+                <div>
+                  <Avatar src={data.user?.userAvatar ?? <UserOutlined />} />
+                </div>
+              </Flex>
+            </Card>
+          </List.Item>
+        )}
+      />
     </PageContainer>
   );
 };
